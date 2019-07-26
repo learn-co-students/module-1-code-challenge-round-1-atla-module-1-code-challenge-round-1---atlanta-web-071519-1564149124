0de73af11f05b4f4bbcd1a778f20bdde03c63192
jonathanboss17 <jtboss47@gmail.com>
2019-07-26 10:57:25 -0400

    jboss

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,58 @@ class Customer
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
+
+  @@all = []
+  @@count = 0
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant: restaurant, customer: self, content: content, rating: rating)
+    @@count += 1
+  end
+
+  def num_reviews
+    # count = 0
+    # Review.all.each do |review|
+    #   if(review.customer == self)
+    #     count += 1
+    #   end
+    # end
+    # count
+    @@count
+  end
+
+  def restaurants 
+    Review.all.map do |review|
+      if(review.customer == self)
+        review.restaurant
+      end
+    end
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Customer.all.select do |customer| 
+      customer.full_name == name
+    end.first
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    Customer.all.map {|customer| customer.full_name}
+  end
+
+
+
 end
