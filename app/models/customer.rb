7352037705eb7bf542c59940219f11aecb8331fc
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-26 11:00:04 -0400

    crushed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,31 @@
 class Customer
+  attr_accessor :restaurant, :content, :rating, :review
   attr_reader :first_name, :last_name
+  @@all = []
+  def initialize(props={})
+    @first_name = props[:first_name]
+    @last_name  = props[:last_name]
+    @restaurant = props[:restaurant]
+    @content = props[:content]
+    @rating = props[:rating]
+    @@all << self
+  end
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+
+  end
+
+  def restaurants(restaurant)
+    restaurant.all.select do |restaurant|
+      restaurant.customer == self
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,21 @@
 class Restaurant
   attr_reader :name
+  @@all = []
+  def initialize(props={})
+    @name = props[:name]
+    @customers = props[:customers]
+    @reviews = props[:reviews]
+    @average_star_rating = props[:average_star_rating]
+    @longest_reviews = props[:longest_reviews]
+    @@all << self
+  end
 
-  def initialize(name)
-    @name = name
+  def self.all
+    @@all
+  end
+
+  def find_by_name(name)
+    name.all.
   end
 
 end
